606eea6bbe94a1ea86e6a927886eaecb0b0ed287
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-07-05 09:02:40 +0900

    Add the observable object.

--- TIL/AddThingView.swift
+++ TIL/AddThingView.swift
@@ -33,28 +33,29 @@
 import SwiftUI
 
 struct AddThingView: View {
-  @Environment(\.presentationMode) var presentationMode
+    @Environment(\.presentationMode) var presentationMode
     @State private var thing = ""
-    @Binding var someThings: ThingStore
-  var body: some View {
-    VStack {
-        TextField("Thing I Learned", text: $thing)
-            .textFieldStyle(.roundedBorder)
-            .padding()
-            .disableAutocorrection(true)
-      Button("Done") {
-          if !thing.isEmpty {
-              someThings.things.append(thing)
-          }
-        presentationMode.wrappedValue.dismiss()
-      }
-      Spacer()
+    @ObservedObject var someThings: ThingStore
+    var body: some View {
+        VStack {
+            TextField("Thing I Learned", text: $thing)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .disableAutocorrection(true)
+            Button("Done") {
+                if !thing.isEmpty {
+                    someThings.things.append(thing)
+                }
+                presentationMode.wrappedValue.dismiss()
+            }
+            Spacer()
+        }
+        .environment(\.textCase, nil)
     }
-  }
 }
 
 struct AddThingView_Previews: PreviewProvider {
-  static var previews: some View {
-      AddThingView(someThings: .constant(ThingStore()))
-  }
+    static var previews: some View {
+        AddThingView(someThings: ThingStore())
+    }
 }
